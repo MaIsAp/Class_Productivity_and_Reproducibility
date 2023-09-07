dcbb7c285cc0f3f866377b537aa842d72b9cb384
Maria Isabel Arce <mi.arceplata@gmail.com>
2023-09-07 11:55:12 -0400

    create example script and download example dataset to save on rawdata

---
+++ 01_scripts/example.R
@@ -0,0 +1,10 @@
+## September 7, 2023
+## Maria Isabel Arce Plata
+## Test script for repo course
+
+library(gapminder)
+library(tidyverse)
+
+View(gapminder)
+
+write_csv(gapminder, "./00_rawdata/gapminder.csv")
